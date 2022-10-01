3097dbd1072ae896074375399175f548849c376c
Sebastian Pezoa Franzani <sebastianpezoa@gmail.com>
2022-10-01 10:47:23 -0300

    trabajo con apis, se crea html y js api-dog

--- api-dog.js
+++ api-dog.js
@@ -1,25 +1,29 @@
 (function () {
-
     function get(url) {
         return Promise.resolve(
             fetch(url)
-                .then((Response) => {
-                    return Response.json();
-                })
-        )
-    };
+                .then((response) => {
+            return response.json();
+        })
+    )
+};
 
-    const url = 'https://dog.ceo/api/breeds/image/random';
+const url = 'https://dog.ceo/api/breeds/image/random';
 
-    async function getDog() {
-        const image = document.querySelector('#doggy');
-        get(url)
+async function getDog() {
+    const image = document.querySelector('#doggy');
+    get(url)
         .then((data) => {
             image.src = data.message;
-        });
-        .catch((err)=> {
-            const contenedor = document.querySelector('#theDog');
-            contenedor.innerHTML += '<p>ERROR</p><p>${err}</p>';
         })
-    }
-})();
+        .catch((err) => {
+        const contenedor = document.querySelector('#theDog');
+        contenedor.innerHTML += '<p>ERROR</p> <p>${err}</p>';
+    });
+};
+
+const button = document.querySelector('#buttonDog');
+button.addEventListener('click', getDog)
+
+getDog()
+}) ();
